1c576941ee1bffb01902772d6d3991ee4320a994
Sachin <sachinadlakha1801@gmail.com>
2025-06-14 21:21:13 -0400

    fix: pass theme to ChakraProvider

--- speech-app/src/App.jsx
+++ speech-app/src/App.jsx
@@ -1,9 +1,11 @@
-import { ChakraProvider, Box, Heading } from '@chakra-ui/react';
+import { ChakraProvider, Box, Heading, extendTheme } from '@chakra-ui/react';
 import SpeechInput from './components/SpeechInput.jsx';
+
+const theme = extendTheme({});
 
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Box p={6} maxW="xl" mx="auto" textAlign="center">
         <Heading mb={4}>Voice Notes</Heading>
         <SpeechInput />
