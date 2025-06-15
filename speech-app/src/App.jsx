1d8d32eb2121fbee12895ead681bdb3a8aa928e5
Sachin <sachinadlakha1801@gmail.com>
2025-06-14 22:37:37 -0400

    style: horizontally center widget with full-width flex box

--- speech-app/src/App.jsx
+++ speech-app/src/App.jsx
@@ -3,7 +3,7 @@ import SpeechInput from './components/SpeechInput.jsx';
 
 function App() {
   return (
-    <Box p={6} maxW="xl" mx="auto" textAlign="center" display="flex" flexDir="column" alignItems="center" justifyContent="center" minH="100vh">
+    <Box p={6} w="100%" textAlign="center" display="flex" flexDir="column" alignItems="center" justifyContent="center" minH="100vh">
       <Heading mb={6} size="lg">Give your voice input below</Heading>
       <SpeechInput />
     </Box>
